538f9b4246665df384dfa8518ec69f84da4c248a
JeffG05 <gugelmann.jeff@gmail.com>
2021-01-30 10:46:49 +0000

    Initial Commit

---
+++ Bluetooth Rower/Bluetooth_RowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_RowerApp.swift
+//  Bluetooth Rower
+//
+//  Created by Jeff Gugelmann on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bluetooth_RowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bluetooth Rower/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bluetooth Rower
+//
+//  Created by Jeff Gugelmann on 30/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
